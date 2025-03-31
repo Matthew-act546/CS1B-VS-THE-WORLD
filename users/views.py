b10bd22a989f9cfc55d12999553834efebdb8192
matthew <matthewyt765@gmail.com>
2025-03-31 21:03:52 +0800

    fix code

--- users/views.py
+++ users/views.py
@@ -4,6 +4,3 @@ from django.http import HttpResponse
 # Create your views here.
 def all_user(request):
   return render(request, "home.html")
-
-def home(request):
-  return render(request, 'index.html')
