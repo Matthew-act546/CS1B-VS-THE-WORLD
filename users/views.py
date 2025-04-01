143e10c5ca8f1f2997349cf2a72eafdfff2036ba
matthew <matthewyt765@gmail.com>
2025-04-01 14:55:59 +0800

    add

--- users/views.py
+++ users/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import User
 
 # Create your views here.
 def all_user(request):
   first_name = ["Matthew", "David", "Fernandez", "Chariss", "Salvador"]
-  context = {"first_name": first_name, "nest": {"nested": 123}}
+
+  users_list = User.objects.all()
+  context = {'users': users_list}
   return render(request, "user.html", context)
