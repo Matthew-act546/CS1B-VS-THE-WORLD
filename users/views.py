6e531cb50927f94c26aa7b5053d3f12a27093010
matthew <matthewyt765@gmail.com>
2025-04-01 11:56:35 +0800

    add nested

--- users/views.py
+++ users/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 def all_user(request):
   first_name = ["Matthew", "David", "Fernandez", "Chariss", "Salvador"]
-  context = {"first_name": first_name}
+  context = {"first_name": first_name, "nest": {"nested": 123}}
   return render(request, "user.html", context)
