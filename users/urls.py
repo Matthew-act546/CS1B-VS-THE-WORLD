5ead8d805e775321ef178f3a83b1ecd3df4219c1
matthew <matthewyt765@gmail.com>
2025-04-03 09:53:52 +0800

    sadfas

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
   # path('', views.all_user, name="users"),
   path('login/', views.login_user, name="login"),
+  path('logout/', views.logout_user, name="logout"),
 ]
--- users/views.py
+++ users/views.py
@@ -16,3 +16,8 @@ def login_user(request):
       return redirect('login')
   else:
     return render(request, "authentication/login.html")
+  
+def logout_user(request):
+  logout(request)
+  messages.success(request, 'you successfully logout')
+  return redirect('home')
